7eb2ffdb598aba2020b6b8e9536fec9e828bd9e4
ninicdennis <pupperbot@protonmail.com>
2021-03-04 17:17:34 -0700

    Added link to game on startup.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -8,7 +8,10 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello</h1>
+      <h1>Untitled Card Game</h1>
+      <Link href ='/game'>
+        <a>Game</a>
+      </Link>
     </div>
   )
 }
