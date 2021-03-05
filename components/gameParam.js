0076bca9de131d25e40afe16acb681809fbcf2cb
ninicdennis <pupperbot@protonmail.com>
2021-03-04 20:05:09 -0700

    Took out boardgame parameter outside of game.js for cleaner code.

---
+++ components/gameParam.js
@@ -0,0 +1,56 @@
+ const GameParam = {
+  setup: () => ({
+    [0]: {
+      deck: [{name: 'test', value: 1, id: 0},{name: 'tester', value: 1, id: 1},{name: 'testing', value: 1, id: 2}],
+      hand: [],
+      board: [null,null,null,null,null,]
+    },
+    [1]: {
+      deck: [{name: 'test', value: 1, id: 0},{name: 'tester', value: 1, id: 1},{name: 'testing', value: 1, id: 2}],
+      hand: [],
+      board: [null,null,null,null,null,]
+    }
+  }),
+  // turn: {
+  //   stages: {
+  //     draw: {
+  //       moves: {
+  //         drawCard: (G, ctx) => {
+  //         console.log('player: ', ctx.currentPlayer)
+  //         if(G[ctx.currentPlayer].deck.length !== 0) {
+  //           const drawNum = Math.floor(Math.random() * G[ctx.currentPlayer].deck.length)
+  //           let card = G[ctx.currentPlayer].deck[drawNum];
+  //           console.log('Card: ', card.name, drawNum)
+  //           G[ctx.currentPlayer].hand.push(card) // Add card to hand.
+  //           G[ctx.currentPlayer].deck.splice(drawNum, 1); // Remove from deck.
+  //         } else {
+  //           // This would be the case if you are out of cards.
+  //           return INVALID_MOVE;
+  //         }
+  //       },}
+  //     }
+  //   }
+  // },
+  moves : {
+    drawCard: (G, ctx) => {
+      console.log('player: ', ctx.currentPlayer)
+      if(G[ctx.currentPlayer].deck.length !== 0) {
+        const drawNum = Math.floor(Math.random() * G[ctx.currentPlayer].deck.length)
+        let card = G[ctx.currentPlayer].deck[drawNum];
+        console.log('Card: ', card.name, drawNum)
+        G[ctx.currentPlayer].hand.push(card) // Add card to hand.
+        G[ctx.currentPlayer].deck.splice(drawNum, 1); // Remove from deck.
+      } else {
+        // This would be the case if you are out of cards.
+        return INVALID_MOVE;
+      }
+    },
+    summonCard: (G, ctx, card, position) => {
+        console.log('Summoning: ', card, position)
+        G[ctx.currentPlayer].hand.splice(card.handPos, 1);
+        G[ctx.currentPlayer].board[position] = card.c;
+    }
+  }
+}
+
+export default GameParam
--- pages/game.js
+++ pages/game.js
@@ -3,66 +3,11 @@ import { INVALID_MOVE } from 'boardgame.io/core';
 import {useState, useEffect} from 'react'
 import {useDrag} from 'react-dnd'
 import { useDrop } from 'react-dnd'
-
-const GameParam = {
-  setup: () => ({
-    [0]: {
-      deck: [{name: 'test', value: 1, id: 0},{name: 'tester', value: 1, id: 1},{name: 'testing', value: 1, id: 2}],
-      hand: [],
-      board: [null,null,null,null,null,]
-    },
-    [1]: {
-      deck: [{name: 'test', value: 1, id: 0},{name: 'tester', value: 1, id: 1},{name: 'testing', value: 1, id: 2}],
-      hand: [],
-      board: [null,null,null,null,null,]
-    }
-  }),
-  // turn: {
-  //   stages: {
-  //     draw: {
-  //       moves: {
-  //         drawCard: (G, ctx) => {
-  //         console.log('player: ', ctx.currentPlayer)
-  //         if(G[ctx.currentPlayer].deck.length !== 0) {
-  //           const drawNum = Math.floor(Math.random() * G[ctx.currentPlayer].deck.length)
-  //           let card = G[ctx.currentPlayer].deck[drawNum];
-  //           console.log('Card: ', card.name, drawNum)
-  //           G[ctx.currentPlayer].hand.push(card) // Add card to hand.
-  //           G[ctx.currentPlayer].deck.splice(drawNum, 1); // Remove from deck.
-  //         } else {
-  //           // This would be the case if you are out of cards.
-  //           return INVALID_MOVE;
-  //         }
-  //       },}
-  //     }
-  //   }
-  // },
-  moves : {
-    drawCard: (G, ctx) => {
-      console.log('player: ', ctx.currentPlayer)
-      if(G[ctx.currentPlayer].deck.length !== 0) {
-        const drawNum = Math.floor(Math.random() * G[ctx.currentPlayer].deck.length)
-        let card = G[ctx.currentPlayer].deck[drawNum];
-        console.log('Card: ', card.name, drawNum)
-        G[ctx.currentPlayer].hand.push(card) // Add card to hand.
-        G[ctx.currentPlayer].deck.splice(drawNum, 1); // Remove from deck.
-      } else {
-        // This would be the case if you are out of cards.
-        return INVALID_MOVE;
-      }
-    },
-    summonCard: (G, ctx, card, position) => {
-        console.log('Summoning: ', card, position)
-        G[ctx.currentPlayer].hand.splice(card.handPos, 1);
-        G[ctx.currentPlayer].board[position] = card.c;
-    }
-  }
-}
+import GameParam from '../components/gameParam'
+let cardInMovement = {}
 const ItemTypes = {
   CARD: 'card'
 }
-
-let cardInMovement = {}
 
 const Card = ({c, handPos, cp, ctx}) => {
   const [{isDragging}, drag] = useDrag(() => ({
@@ -117,14 +62,12 @@ const SummonBoard = ({x,y,children, position, moves, playerType,ctx, cp}) => {
                 backgroundColor: 'yellow',
               }}
             />
-      )}
+           )}
           </div>
         )
-    
       }
 
 const CardInPlay = ({c}) => {
-  console.log(c)
   return (
     <div>
       <div style = {{width: 200, height: 300, backgroundColor: '#424242', margin: 10}}>
