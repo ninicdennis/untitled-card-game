499c713dfec16a61e9c73f77dbd4f42f6e5c1952
ninicdennis <pupperbot@protonmail.com>
2021-03-07 11:45:31 -0700

    Fixed weird rendering but where an error would make the icons not able to be imported. Node module import directly fixes this for some odd reason.

--- components/cardInPlay.js
+++ components/cardInPlay.js
@@ -1,8 +1,7 @@
 import {Icon} from '@iconify/react';
-import swordIcon from '@iconify-icons/mdi/sword'
-import shieldIcon from '@iconify-icons/mdi/shield'
-import hexagon from '@iconify-icons/mdi/hexagon-slice-3'
-
+import swordIcon from '../node_modules/@iconify-icons/mdi/sword'
+import shieldIcon from '../node_modules/@iconify-icons/mdi/shield'
+import hexagon from '../node_modules/@iconify-icons/mdi/hexagon-slice-3'
 const CardInPlay = ({position, c, confirmAttack, player}) => {
   return (
     <div style = {{width: 200, height: 300, backgroundColor: '#424242', margin: 10,
